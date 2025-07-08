40eb94133807f352f5ae0fe8bde96e33b17467c0
Rituraj <rkthakur.1102@gmail.com>
2025-07-09 00:12:21 +0530

    fix overflow width

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
            <div className="heading">
              <h2 className="title-head">Proj<span className="bg-span">ects</span></h2>
            </div>
-           <div className="container">
+           <div className="container" style={{maxWidth: "100vw", overflowX: "scroll"}}>
              {
                 projectsData.map((data) => (
                   <Card data={data} key={data.id} />
